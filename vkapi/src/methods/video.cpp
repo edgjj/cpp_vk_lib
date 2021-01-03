8673d30f986fd4fc44f8e29c3b765f4333b2d44b
oxfffffe <davydkar77@gmail.com>
2021-01-04 00:41:27 +0100

    error handling

--- vkapi/src/methods/video.cpp
+++
@@ -1,6 +0,0 @@
-#include "vkapi/include/methods/video.hpp"
-
-vk::attachment::attachment_list vk::video::search(std::string_view query, std::int64_t count) const
-{
-  return common_search("video.search", query, count);
-}
