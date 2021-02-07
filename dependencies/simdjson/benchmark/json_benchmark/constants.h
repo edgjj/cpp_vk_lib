929674af0c96fb7693d32592ac29f5d9c496b207
oxfffffe <davydkar77@gmail.com>
2021-02-07 17:20:22 +0100

    removed: submodules

--- dependencies/simdjson/benchmark/json_benchmark/constants.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace json_benchmark {
-
-static constexpr const char *TWITTER_JSON = SIMDJSON_BENCHMARK_DATA_DIR "twitter.json";
-static constexpr const char *NUMBERS_JSON = SIMDJSON_BENCHMARK_DATA_DIR "numbers.json";
-
-}
