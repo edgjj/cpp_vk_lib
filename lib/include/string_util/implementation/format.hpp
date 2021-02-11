9695cae0007aada44dc912441629bbaee04c985e
oxfffffe <davydkar77@gmail.com>
2021-02-11 21:36:07 +0100

    updated: string format util

--- lib/include/string_util/implementation/format.hpp
+++ lib/include/string_util/implementation/format.hpp
@@ -2,6 +2,7 @@
 #define STRING_UTIL_FORMAT_IMPL_H
 
 #include <deque>
+#include <array>
 #include <sstream>
 #include <any>
 
@@ -17,39 +18,27 @@ std::string format(std::string_view str, Args&&... args);
 
 namespace vk {
 namespace string_util {
-struct vk_hidden any_stream : public std::any {
-    void(*streamer)(std::ostream&, any_stream const&) = nullptr;
-    friend std::ostream& operator<<(std::ostream& os, const any_stream& a) {
-        a.streamer(os, a);
-        return os;
-    }
-    template<class T, std::enable_if_t<!std::is_same<std::decay_t<T>, any_stream>{}, bool> = true>
-    any_stream(T&& t)
-        : std::any(std::forward<T>(t))
-        , streamer([](std::ostream& os, const any_stream& self) {
-            os << std::any_cast<std::decay_t<T>>(self);
-        })
-    { }
-};
-
 template <typename... Args> struct vk_hidden format_implementation {
 private:
-    static void pack(std::deque<any_stream>& container, Args&&... args) {
-        ([&container](auto& input){ container.push_back(input); } (args), ...);
-    }
     static std::string create(std::string_view str, Args&&... args) {
-        std::stringstream formatted;
-        std::deque<any_stream> elements;
-        pack(elements, std::forward<Args>(args)...);
+        std::string formatted;
+        std::ostringstream ss;
+        auto packOne = [&ss](auto&& arg) {
+            ss.str("");
+            ss.clear();
+            ss << std::forward<decltype(arg)>(arg);
+            return ss.str();
+        };
+        std::array<std::string, sizeof...(Args)> elements{packOne(args)...};
+        std::size_t curr = 0;
         for (std::size_t i = 0; i < str.size(); i++) {
             if (str[i] == '{' && str[i + 1] == '}') {
-                formatted << elements.front();
-                elements.pop_front();
+                formatted += elements[curr++];
             } else if (str[i - 1] != '{' || str[i] != '}') {
-                formatted << str[i];
+                formatted += str[i];
             }
         }
-        return formatted.str();
+        return formatted;
     }
 
     template <typename... _Args>
