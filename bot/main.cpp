523c88ef19af6fcea96ea4ba44bbdb1464ec08ac
oxfffffe <davydkar77@gmail.com>
2021-02-11 17:40:11 +0100

    updated: example bot

--- bot/main.cpp
+++ bot/main.cpp
@@ -8,7 +8,6 @@
 class message_new_handler {
 public:
     using event_wrapper = std::function<void(const vk::event::message_new&)>;
-
     void on_command(std::string_view trigger, event_wrapper function) {
         commands.emplace(trigger, function);
     }
@@ -16,42 +15,47 @@ public:
         commands.at(trigger)(event);
     }
     void see_contents() {
-        for (auto&& command : commands) {
+        for (auto&& command : commands)
             logger(logflag::debug | logflag::green) << "loaded command: " << command.first;
-        }
         logger(logflag::debug | logflag::green) << "total commands: " << commands.size();
     }
-
 private:
     std::unordered_map<std::string_view, event_wrapper> commands;
 };
 
-int main() {
-    vk::messages messages;
-    vk::long_poll_api long_poll;
-    vk::long_poll_data long_poll_data = long_poll.server();
-    message_new_handler handler;
-
-    handler.on_command("1", [&messages](const vk::event::message_new& event){
-        messages.send(event.peer_id(), "command 1", {
-            {"attachment", "photo123_123"}
-        });
-    });
-    handler.on_command("2", [&messages](const vk::event::message_new& event){
-        messages.send(event.peer_id(), "command 1", {
-            {"attachment", "video123_123"}
-        });
-    });
-
-    while (true) {
-        auto events = long_poll.listen(long_poll_data);
-        std::for_each(std::begin(events), std::end(events), [&](auto& common_event){
-            if (common_event->on_type("message_new")) {
-                long_poll.queue([event = common_event->get_message_event(), &handler](){
-                    handler.try_execute(event.text(), event);
-                });
-            }
-        });
-        long_poll.run();
+class long_poller {
+public:
+    explicit long_poller() : data(api.server()) { }
+    using message_event_wrapper = std::function<void(const vk::event::message_new&)>;
+    void on_message_event(std::string_view trigger, message_event_wrapper function) {
+        handler.on_command(trigger, function);
     }
+    void loop() {
+        while (true) {
+            auto events = api.listen(data);
+            std::for_each(events.begin(), events.end(), [this](auto& event){
+                if (event->on_type("message_new")) {
+                    api.queue([this, e = event->get_message_event()]{
+                        handler.try_execute(e.text(), e);
+                    });
+                }
+                data.ts = event->ts();
+            });
+            api.run();
+        }
+    }
+private:
+    message_new_handler handler;
+    vk::long_poll_api api;
+    vk::long_poll_data data;
+};
+
+int main() {
+    long_poller poller;
+    vk::messages messages;
+    poller.on_message_event("text", [&messages](const vk::event::message_new& event){
+        messages.send(event.peer_id(), vk::string_util::format("Message: {}.", event.text()));
+    });
+    poller.loop();
+    return 0;
 }
