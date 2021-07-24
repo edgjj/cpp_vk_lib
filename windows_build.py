f65fd25c018124b73a66877c038547d8180be916
threadpoolexecutor <59066701+threadpoolexecutor@users.noreply.github.com>
2021-07-24 23:04:41 +0300

    Update windows_build.py

--- windows_build.py
+++ windows_build.py
@@ -3,11 +3,14 @@ import sys
 import subprocess
 
 vcpkg_path = sys.argv[1]
+
 if not os.path.exists(vcpkg_path):
 	print("Vcpkg path is invalid")
 	exit()
 triplet = sys.argv[2]
 
-subprocess.call("mkdir ./windows_build", shell=False)
+absolute_path = os.path.abspath(os.getcwd())
+
+subprocess.call(f"mkdir {absolute_path}/windows_build", shell=True)
 os.chdir("windows_build")
-subprocess.call(f'cmake .. -DCMAKE_TOOLCHAIN_FILE="{vcpkg_path}/scripts/buildsystems/vcpkg.cmake" -DVCPKG_TARGET_TRIPLET="{triplet}"', shell=False)
+subprocess.call(f'cmake .. -DCMAKE_TOOLCHAIN_FILE="{vcpkg_path}/scripts/buildsystems/vcpkg.cmake" -DVCPKG_TARGET_TRIPLET="{triplet}"', shell=True)
