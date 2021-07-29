c0e43c106b4addc4f3043a31b28b1d15ecf3de51
threadpoolexecutor <davidkor77@gmail.com>
2021-07-30 00:00:29 +0300

    added CURL test

---
+++ test/curl_tests.cpp
@@ -0,0 +1,27 @@
+#include "cpp_vk_lib/net/network.hpp"
+
+#include <gtest/gtest.h>
+
+#include <regex>
+
+TEST(curl, POST)
+{
+    try {
+        const std::string received = vk::network::request("https://www.example.com");
+        if (received.empty()) {
+            ASSERT_FALSE(true);
+        } else {
+            if (received.find("Example Domain") == std::string::npos) {
+                ASSERT_FALSE(true);
+            }
+        }
+    } catch (...) {
+        ASSERT_FALSE(true);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
