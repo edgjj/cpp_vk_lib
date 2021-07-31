731d5b617adbd51bb1dbea4449112c8cb57d39ae
threadpoolexecutor <davidkor77@gmail.com>
2021-07-31 15:18:44 +0300

    added testing module to CMake

---
+++ cmake/testing.cmake
@@ -0,0 +1,17 @@
+find_library(cpp_vk_lib HINTS "${CMAKE_CURRENT_SOURCE_DIR}/lib")
+include_directories(lib)
+include_directories(dependencies/spdlog/include)
+
+enable_testing()
+
+function (add_vk_test test_name test_path)
+  add_executable(${test_name} ${test_path})
+  target_link_libraries(${test_name} cpp_vk_lib gtest)
+  add_test(${test_name} ${test_name})
+endfunction()
+
+file (GLOB test_files "${CMAKE_CURRENT_SOURCE_DIR}/test/*.cpp")
+foreach (file ${test_files})
+  get_filename_component(test_name ${file} NAME_WE)
+  add_vk_test(${test_name} ${file})
+endforeach()
