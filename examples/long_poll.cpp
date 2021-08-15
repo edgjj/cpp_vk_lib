e783b950a2a060512922c87eb68d6001cc988876
threadpoolexecutor <davidkor77@gmail.com>
2021-08-16 02:18:28 +0300

    update long poll example

--- examples/long_poll.cpp
+++ examples/long_poll.cpp
@@ -8,27 +8,15 @@
 
 #include <iostream>
 
-constexpr char sample_config[] = R"__(
-    {
-      "api": {
-        "access_token": "7483c78eba756697ae4727e5d255d7782226d8bf4dc1a87924897e23278fddd46512c759c960a6468e4cf",
-        "user_token": "not used"
-      },
-      "oauth": {
-        "login": "not used",
-        "password": "not used"
-      },
-      "environment": {
-        "num_workers": 8,
-        "log_path": "logs.txt"
-      }
-    }
-)__";
-
-int main()
+int main(int argc, char* argv[])
 {
+    if (argc != 2) {
+        std::cerr << "Usage: ./long_poll <config.json>" << std::endl;
+        return 1;
+    }
+
     vk::setup_logger("info");
-    vk::config::load_string(sample_config);
+    vk::config::load(argv[1]);
 
     asio::io_context io_context;
     vk::long_poll api(io_context);
