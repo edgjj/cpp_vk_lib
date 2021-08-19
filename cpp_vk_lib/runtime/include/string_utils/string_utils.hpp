28715b3bd9e7a332dab1aada9798d254fabcb5b7
threadpoolexecutor <davidkor77@gmail.com>
2021-08-19 23:49:12 +0300

    added missing split by strings function

--- cpp_vk_lib/runtime/include/string_utils/string_utils.hpp
+++ cpp_vk_lib/runtime/include/string_utils/string_utils.hpp
@@ -22,6 +22,7 @@ template <typename... Args>
 VK_REALLY_INLINE std::string format(std::string_view data, Args&&... args) { return implementation::format(data, std::forward<Args>(args)...); }
 
 VK_REALLY_INLINE std::vector<std::string_view> split(std::string_view data, char delimiter) { return implementation::split_implementation(data, delimiter); }
+VK_REALLY_INLINE std::vector<std::string_view> split(std::string_view data, std::string_view delimiter) { return implementation::split_implementation(data, delimiter); }
 VK_REALLY_INLINE std::vector<std::string_view> whitespace_split(std::string_view data) { return implementation::split_implementation(data, " \f\n\r\t\v"); }
 VK_REALLY_INLINE split_range<std::string_view> lazy_split(std::string_view data, std::string_view delimiter) { return split_range<std::string_view>(data, delimiter); }
 
--- test/string_util_tests.cpp
+++ test/string_util_tests.cpp
@@ -112,7 +112,7 @@ TEST(join, array)
     ASSERT_EQ(util::join<int>(int_container, ','), "1,2,3");
 }
 
-TEST(split, split)
+TEST(split, split_by_symbol)
 {
     const std::vector<std::string_view> v1{"1", "2", "3"};
     const std::vector<std::string_view> v2{"!", "?", ":"};
@@ -125,6 +125,21 @@ TEST(split, split)
     ASSERT_EQ(util::split("1", ' '), v3);
     ASSERT_EQ(util::split("1?1?1?1", '?'), v4);
     ASSERT_EQ(util::split("text1,text2,text3", ','), v5);
+}
+
+TEST(split, split_by_string)
+{
+    const std::vector<std::string_view> v1{"1", "2", "3"};
+    const std::vector<std::string_view> v2{"!", "?", ":"};
+    const std::vector<std::string_view> v3{"1"};
+    const std::vector<std::string_view> v4{"1", "1", "1", "1"};
+    const std::vector<std::string_view> v5{"text1", "text2", "text3"};
+
+    ASSERT_EQ(util::split("1!?2!?3", "!?"), v1);
+    ASSERT_EQ(util::split("!\\\\?\\\\:", "\\\\"), v2);
+    ASSERT_EQ(util::split("1", " "), v3);
+    ASSERT_EQ(util::split("1??1??1??1", "??"), v4);
+    ASSERT_EQ(util::split("text1,,,text2,,,text3", ",,,"), v5);
 }
 
 TEST(split, whitespace_split)
