c2ed153e7afa2bcc70255423407fd66c9919ecd0
threadpoolexecutor <davidkor77@gmail.com>
2021-08-27 23:07:39 +0300

    add Android stacktrace implementation

--- cpp_vk_lib/src/runtime/stacktrace_dump.cpp
+++ cpp_vk_lib/src/runtime/stacktrace_dump.cpp
@@ -2,15 +2,24 @@
 
 #include "spdlog/spdlog.h"
 
-#include <cxxabi.h>
-#include <execinfo.h>
-
-void runtime::stacktrace_dump()
-{
-    // clang-format off
 #if defined(__unix__ ) || defined(__unix) || \
    (defined(__APPLE__) && defined(__MACH__))
-    void* address_list[100];
+#include <cxxabi.h>
+#include <execinfo.h>
+#elif defined(__ANDROID__)
+#include <sstream>
+#include <iomanip>
+#include <android/log.h>
+#include <unwind.h>
+#include <dlfcn.h>
+#endif // defined __unix__ || defined __APPLE__
+
+#if defined(__unix__ ) || defined(__unix) || \
+   (defined(__APPLE__) && defined(__MACH__))
+static void unix_stacktrace_dump_implementation()
+{
+    const size_t max_stacktrace_records = 25;
+    void* address_list[max_stacktrace_records];
     int addresses_got = backtrace(address_list, sizeof(address_list) / sizeof(void*));
     if (addresses_got == 0) {
         spdlog::critical("  empty stack trace, exiting...");
@@ -49,6 +58,65 @@ void runtime::stacktrace_dump()
     }
     free(funcname);
     free(symbol_list);
+}
+#endif // defined __unix__ || defined __APPLE__
+
+#if defined(__ANDROID__)
+struct backtrace_state
+{
+    void** current;
+    void** end;
+};
+
+static _Unwind_Reason_Code unwind_callback(struct _Unwind_Context* context, void* arg)
+{
+    backtrace_state* state = static_cast<backtrace_state*>(arg);
+    uintptr_t pc = _Unwind_GetIP(context);
+    if (pc) {
+        if (state->current == state->end) {
+            return _URC_END_OF_STACK;
+        } else {
+            *state->current++ = reinterpret_cast<void*>(pc);
+        }
+    }
+    return _URC_NO_REASON;
+}
+
+static size_t capture_stacktrace(void** buffer, size_t max)
+{
+    backtrace_state state { buffer, buffer + max };
+    _Unwind_Backtrace(unwind_callback, &state);
+    return state.current - buffer;
+}
+
+static void android_stacktrace_dump_implementation()
+{
+    const size_t max_stacktrace_records = 25;
+    void* buffer[max_stacktrace_records];
+    std::ostringstream ostream;
+
+    const size_t stacktrace_size = capture_stacktrace(buffer, max_stacktrace_records);
+    for (size_t i = 0; i < stacktrace_size; ++i) {
+        const void* address = buffer[i];
+        const char* symbol = "";
+        DL_info info;
+        if (dladdr(address, &info) && info.dli_sname) {
+            symbol = info.dli_sname;
+        }
+        ostream << "  " << std::setw(2) << ": " << address << " " << symbol << std::endl;
+    }
+    spdlog::critical("{}", ostream.str());
+}
+#endif// defined(__ANDROID__)
+
+void runtime::stacktrace_dump()
+{
+    // clang-format off
+#if defined(__unix__ ) || defined(__unix) || \
+   (defined(__APPLE__) && defined(__MACH__))
+    unix_stacktrace_dump_implementation();
+#elif defined(__ANDROID__)
+    android_stacktrace_dump_implementation();
 #endif// defined __unix__ || defined __APPLE__
     // clang-format on
 }
